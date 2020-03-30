13e2f49d4509986968d9f351b231d3bdb7cba917
zengjinlin <754259801@qq.com>
2020-03-30 10:23:09 +0800

    Initial commit

---
+++ src/master.java
@@ -0,0 +1,9 @@
+/**
+ * @author zengjinlin
+ * @create 2020-03-30 0:52
+ */
+public class master {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
