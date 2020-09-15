8a5c2740e0a26482d424ead0860ad7e5ccb00701
zengjinlin <754259801@qq.com>
2020-09-15 22:50:02 +0800

    test1

--- src/master.java
+++ src/master.java
@@ -5,6 +5,6 @@
 public class master {
     public static void main(String[] args) {
         System.out.println("Hello World!aaabbbbffffvv");
-        System.out.println("Hello World!aaabbbbffffvv");
+        System.out.println("Hello World!1111");
     }
 }
